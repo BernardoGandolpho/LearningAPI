972fdd99af20e362e85203623706ca106aa1f535
BernardoGandolpho <begandolpho@gmail.com>
2022-05-05 11:45:29 -0300

    Query, Path e Body

--- fastapi/README.txt
+++
@@ -1,8 +0,0 @@
-Ativando ambiente virtual
-$ source venv/bin/activate
-
-Executando a aplicação
-$ uvicorn main:app --reload
-    * main -> main.py
-    * app -> app = FastAPI()
-    * --reload -> reload after code changes
--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,31 @@
-from typing import Optional
+from typing import Set, Optional
 from enum import Enum
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel, Field, HttpUrl
+
+
+# Models and predefined items
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = Field(
+        None, title="The description of the item", max_length=300
+    )
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
+    tax: Optional[float] = None
+    tags: Set[str] = []
+    image: Optional[Image] = None
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 app = FastAPI()
@@ -27,12 +52,6 @@ async def read_user_item(
     return item
 
 
-# Predefined items
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name == ModelName.alexnet:
@@ -42,3 +61,38 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/items/")
+async def read_items(
+    q: Optional[str] = Query(
+        None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+    )
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item(
+        *, item_id: int = Path(..., ge=1),
+        item: Item,
+        q: Optional[str] = Query(None, min_lenght=3)):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
